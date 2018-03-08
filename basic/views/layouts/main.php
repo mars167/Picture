301ca464a9cdbfca6909daacce3b3b94153d0dc6
mars167 <mars167@icloud.com>
2018-03-08 23:22:44 +0800

    数据库文件更新

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -67,7 +67,9 @@ AppAsset::register($this);
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
+
         <?= $content ?>
+
     </div>
 </div>
 
